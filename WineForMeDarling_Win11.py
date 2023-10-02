6c396cbe4c446573223339523255fe51994d94ae
May Yablonko <123502026+MayYablonko@users.noreply.github.com>
2023-10-02 10:35:47 +0300

    version with toast notif for win11
    same exact code, but updated to fit Windows 11 toast notifications.

---
+++ WineForMeDarling_Win11.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from win11toast import toast
+import time
+
+
+def loadMyWines():
+
+    info = { 
+        'url': 'https://wine-he.com/',
+        'name': 'your name',
+        'phone': 'your phone',
+        'email': 'your email',
+        'sum': 0,
+        'card': 'tenbis',
+        'card_number': 0
+    }
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(info['url'])
+
+    time.sleep(5)
+
+    nameField = driver.find_element(By.ID, "form-field-name") # text
+    phoneField = driver.find_element(By.ID, "form-field-field_3a7272e") # text
+    emailField = driver.find_element(By.ID, "form-field-email") # text
+    sumField = driver.find_element(By.ID, "form-field-field_140acd2") # text
+    card_field = driver.find_element(By.ID, "form-field-field_2eb71e4-0") # choose one
+    cardNumberField = driver.find_element(By.ID, "form-field-field_cad2223") # text
+    submit_button = driver.find_element(by=By.CLASS_NAME, value="elementor-button") # button
+
+    nameField.send_keys(info['name'])
+    phoneField.send_keys(info['phone'])
+    emailField.send_keys(info['email'])
+    sumField.send_keys(info['sum'])
+    card_field.click()
+    cardNumberField.send_keys(info['card_number'])
+    submit_button.click()
+
+    time.sleep(20)
+
+    message = driver.find_element(by=By.CLASS_NAME, value="elementor-message-success")
+    value = message.text
+    
+    if "בהצלחה" in value:
+        toast("WineForMeDarling", "Success!", duration = "long", icon = None,)
+    else:
+        toast("WineForMeDarling", "Failed!", duration = "long", icon = None,)
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+     loadMyWines()
+
